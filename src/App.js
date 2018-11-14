75d113f670d9cd5de1b447938ae11c1d3ee492f6
PavlyukVadim <pavlyuk.dev@gmail.com>
2018-11-14 17:53:22 +0200

    initial redux setup

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { connect } from 'react-redux';
+import { simpleAction } from './actions/simpleAction'
+
+import logo from './assets/logo.svg';
 import './App.css';
 
+const mapStateToProps = state => ({
+  ...state
+});
+
+const mapDispatchToProps = dispatch => ({
+  simpleAction: () => dispatch(simpleAction())
+});
+
 class App extends Component {
+  simpleAction = (event) => {
+    this.props.simpleAction();
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <pre>
+           {
+             JSON.stringify(this.props)
+           }
+          </pre>
+          <button onClick={this.simpleAction}>Test redux action</button>
         </header>
       </div>
     );
   }
 }
 
-export default App;
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(App);
